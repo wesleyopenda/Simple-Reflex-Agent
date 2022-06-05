1f43f33e7761ec3d2b0692393b8663663406cbb3
wesleyopenda <wopenda.wo@gmail.com>
2022-06-05 04:08:29 +0300

    creating the Agent class and checking the condition of where the vacuum is.

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import random
 
-class Environment(object):
+class Environment(object): # create the environmrnt
     def __init__(self):
         self.location = ['a', 'b']
         self.location_condition = {'a':'0',
@@ -8,3 +8,13 @@ class Environment(object):
         self.vacuum_location = random.choice(self.location)
         self.location_condition['a'] = random.randint(0, 1)
         self.location_condition['b'] = random.randint(0, 1)
+
+class Agent(Environment): # create the agent
+    def __init__(self, Environment):
+        print('Vacuum location: ', Environment.vacuum_location) # find the vacuum
+        print('Location condition: ', Environment.location_condition) # show the condition of the area
+
+        count = 0
+        while count < 2:
+            if Environment.location_condition[Environment.vacuum_location] == 1:
+                Environment.location_condition[Environment.vacuum_location] = 0
